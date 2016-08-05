19ccdcc3c2d1d81539197592a1ea4fa130ba4d0f
Sean Chen <seanchen47@gmail.com>
2016-08-06 02:00:10 +0800

    Update_CUSMAS1 updated

--- methods/Update_CUSMAS1.php
+++ methods/Update_CUSMAS1.php
@@ -1,21 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-	<meta charset="utf-8">
-	<title>三三社企-修改資料</title>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='css/sign.css' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
 
-	<!-- Google Fonts -->
-	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
-
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Custom Stylesheet -->
-	<link rel="stylesheet" href="css/style.css">
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <title>三三社企-修改資料</title>
 </head>
 
 <body>
+<br>
 	<?php
 	session_start();
 	include("Helper/mysql_connect.php");
@@ -25,68 +20,37 @@
 
 	if($EMAIL != null){
 		$row = select('CUSMAS', 'EMAIL', $EMAIL);
-    ?>
-	<div class="container">
-		<div class="top">
-			<!--
-			<h1 id="title" class="hidden"><span id="logo">Daily <span>UI</span></span></h1>
-			-->
-		</div>
-		<div class="login-box animated fadeInUp">
-			<div class="box-header">
-				<h2>修改資料</h2>
-			</div>
+		$CUSNM = $row['CUSNM'];
+		$CUSADD = $row['CUSADD'];
+		$TEL = $row['TEL'];
+		$TAXID = $row['TAXID'];
+		$CUSTYPE = $row['CUSTYPE'];
+		$SPEINS = $row['SPEINS'];
+		function checkCUSTYPE($ori, $new){
+			if($ori == $new) echo 'selected="selected"';
+		}
+	?>
+	<div class="sign-block">
+    	<h1>修改資料</h1>
 			<form name="form" method="post" action="Update_CUSMAS_finish.php">
-			<label for="username">
-			<?php
-				echo "電子信箱：$EMAIL <p>";
-			?>
-			<label for="username">姓名
-	        <?php
-	        $CUSNM = $row['CUSNM'];
-	        echo "<input type=\"text\" name=\"CUSNM\" value=\"$CUSNM\" />";
-	        ?>
-	        地址
-	        <?php
-	        $CUSADD = $row['CUSADD'];
-	        echo "<input type=\"text\" name=\"CUSADD\" value=\"$CUSADD\" /> <br>";
-	        ?>
-	    	</label><br>
-	    	<label for="username">電話
-	        <?php
-	        $TEL = $row['TEL'];
-	        echo "<input type=\"text\" name=\"TEL\" value=\"$TEL\" />";
-	        ?>
-	        統編
-	        <?php
-	        $TAXID = $row['TAXID'];
-	        echo "<input type=\"text\" name=\"TAXID\" value=\"$TAXID\" />";
-	        ?>
-	    	</label><br>
-	        <label for="username">
-					<div class="styled-select">您的膚質*<select name="CUSTYPE"
-						<?php
-							$CUSTYPE = $row['CUSTYPE'];
-							echo "$CUSTYPE";
-						?>
-						></option>
-					  	<option value="A">乾性</option>
-					  	<option value="B">中性</option>
-					  	<option value="C">油性</option>
-					  	<option value="D">混和性</option>
-					</select></div>
-				</label>
-	        <label for="username">特殊要求
-	        	<?php
-	        		$SPEINS = $row['SPEINS'];
-	        		echo "<textarea name=\"SPEINS\" cols=\"45\" rows=\"5\">$SPEINS</textarea>";
-	        	?>
-	        </label><br>
-	        
-	        <button type="submit">確定</button>
-	        </form>
-			<button type="button"></buttom><a href="../Homepage/index.php">取消</a>
-		</div>
+				<label for="username">電子信箱：<?echo $EMAIL;?></label><br>
+				<label for="username">您的姓名：<input type="text" name="CUSNM" value="<?echo $CUSNM;?>"/></label><br>
+				<label for="username">通訊地址：<input type="text" name="CUSADD" value="<?echo $CUSADD;?>"/></label><br>
+				<label for="username">聯絡電話：<input type="text" name="TEL" value="<?echo $TEL;?>"/></label><br>
+				<label for="username">統一編號：<input type="text" name="TAXID" value="<?echo $TAXID;?>"/></label><br>
+				<div class="styled-select">您的膚質：
+					<select name="CUSTYPE">
+					  	<option value="A" <?checkCUSTYPE($CUSTYPE, 'A');?>>乾性</option>
+					  	<option value="B" <?checkCUSTYPE($CUSTYPE, 'B');?>>中性</option>
+					  	<option value="C" <?checkCUSTYPE($CUSTYPE, 'C');?>>油性</option>
+					  	<option value="D" <?checkCUSTYPE($CUSTYPE, 'D');?>>混和性</option>
+					</select>
+				</div>
+				</label><br>
+				<label for="username">特殊要求：<textarea name="SPEINS" cols="45" rows="5"><?echo $SPEINS;?></textarea></label><br>
+				<button type="submit" class="promise">確定</button>
+			</form>
+		<button type="button" class="cancel"></buttom><a href="../Homepage/index.php">取消</a>
 	</div>
 	<?php
 	}
