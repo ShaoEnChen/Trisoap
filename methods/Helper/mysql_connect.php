8822ba1265f4019d43afb0a26270c55aa6665d49
seanchen47 <seanchen47@gmail.com>
2016-10-27 08:43:00 +0000

    php 7.0 mysql support - function modified

--- methods/Helper/mysql_connect.php
+++ methods/Helper/mysql_connect.php
@@ -2,13 +2,13 @@
 $db_server = "localhost";
 $db_name = "Trisoap";
 $db_user = "root";
-$db_passwd = "";
+$db_passwd = "3b4b6b187def8c13";
 
-if(!@mysql_connect($db_server, $db_user, $db_passwd))
+if(!@mysqli_connect($db_server, $db_user, $db_passwd, $db_name))
         die("無法對資料庫連線");
 
-mysql_query("SET NAMES utf8");
+// mysql_query("SET NAMES utf8");
 
-if(!@mysql_select_db($db_name))
-        die("無法使用資料庫");
+// if(!@mysql_select_db($db_name))
+// 	die("無法使用資料庫");
 ?>
