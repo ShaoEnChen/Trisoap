4a92d3b603ab9a81944282fa4bfd5e5fbc5200a7
Tim <a0922825881@gmail.com>
2017-04-28 00:48:55 +0800

    item set completed

--- methods/Delete_ITEMMAS.php
+++ methods/Delete_ITEMMAS.php
@@ -33,6 +33,10 @@ if($EMAIL != null && $CUSIDT == 'A'){
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
         </select>
     </div>
     請再次輸入您的密碼：<input type="password" name="PW" />
