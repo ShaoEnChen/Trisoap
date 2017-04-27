eee8e9f8a61ed4be0711cbb3a95408d820e1176f
Tim <a0922825881@gmail.com>
2017-04-28 00:20:01 +0800

    products backend attached

--- Homepage/product7.php
+++ Homepage/product7.php
@@ -127,7 +127,7 @@
                 <div class="form-group">
                   <label for="number"></label>
                   <input id="number" type="number" name="ORDAMT" value="1" max="50" min="1" class="form-control">
-                  <input type="hidden" name="ITEMNO" value="5">
+                  <input type="hidden" name="ITEMNO" value="7">
                 </div>
                 <button type="submit" class="btn btn-dark">加入購物車</button>
               </form>
